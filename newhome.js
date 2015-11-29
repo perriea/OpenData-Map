af8dd483d283db2c46ceae962f4394219a563771
perriea <perrie_a@etna-alternance.net>
2015-11-29 12:02:08 +0100

    Update newhome.js

--- newhome.js
+++ newhome.js
@@ -76,7 +76,7 @@ function Checked (mapCluster, L)
 */
 function Localisation (mapCluster) 
 {
-  mapCluster.locate({setView: true, watch: false, maxZoom: 18, timeout: 7000})
+  mapCluster.locate({setView: true, watch: false, maxZoom: 1, timeout: 7000})
   .on('locationfound', function(e)
   {
     L.marker(e.latlng).addTo(mapCluster)
